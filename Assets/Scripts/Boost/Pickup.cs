2f76924a83cfc21d6a4386923e4f8526a98322ef
Buck Young <buckcyoung@gmail.com>
2016-10-29 13:50:27 -0400

    Boost recharge + color tinting

--- Assets/Scripts/Boost/Pickup.cs
+++ Assets/Scripts/Boost/Pickup.cs
@@ -9,11 +9,10 @@ namespace RL2600.Boost {
 
 	public class Pickup : MonoBehaviour {
 		private const float MAX_AMT = 35;
-		private const int RECHARGE_TIME = 5; // boostpad will be recharged over this many seconds
+		private const int RECHARGE_TIME = 5; // Boostpad will be recharged over this many seconds
 
 		private Renderer r;
 
-		private bool isInactive = false; 
 		private float amount = 35;
 
 		private Coroutine rechargeCoroutine;
@@ -21,7 +20,7 @@ namespace RL2600.Boost {
 		void Start () {
 			r = GetComponent<Renderer>();
 
-//			r.material.SetColor("_TintColor", Color); // TODO 
+			resetPickup();
 		}
 
 		// OnStay so you can hang out over the boost pad and continually collect
@@ -30,84 +29,103 @@ namespace RL2600.Boost {
 				int id = other.gameObject.GetComponentInParent<Player.Player>().id;
 
 				if (BoostManager.canPickupBoost(id)) {
-					// Stop any existing coroutines 
-					if (rechargeCoroutine != null) {
-						StopCoroutine(rechargeCoroutine);
-					}
-
-					// Pickup the boost
 					pickup(id);
-
-					// Begin recharging boost pad
-					rechargeCoroutine = StartCoroutine(recharge());
 				}
 			}
 		}
 
 		// AI uses this to determine boostpad target
 		public bool getIsActive() {
-			return !isInactive;
+			return amount != 0;
 		}
 
 		// After goal
 		public void resetPickup() {
-			setIsInactive(false);
+			stopRechargeCoroutine();
+
 			amount = MAX_AMT;
+
+			setColor();
 		}
 
 		// private 
 
-		private void setIsInactive(bool value) {
-			isInactive = value;
-			Debug.Log(isInactive);
+		// The min/max/range here is used to exagerate the boost regen process
+		// The colors will be in a small scale in the middle of the color line
+		// Then POP into full boost color when necessary
+		private void setColor() {
+			float min = 0.28f; // ~70
+			float max = 0.68f; // ~170
+			float range = max - min; // 0.40f
+
+			float charge = amount / MAX_AMT; // percentage
+
+			float tint = charge * range + min;
+
+			// Jump to 255 on full charge
+			if (charge == 1.0f) {
+				tint = 1.0f;
+			}
+
+			r.material.SetColor("_TintColor", new Color(tint, tint, tint, 0.5f));
+		}
+
+		private void stopRechargeCoroutine() {
+			if (rechargeCoroutine != null) {
+				StopCoroutine(rechargeCoroutine);
+				rechargeCoroutine = null;
+			}
 		}
 
 		private void pickup(int id) {
+			// Stop existing coroutine
+			stopRechargeCoroutine();
+
+			// Pickup
 			BoostManager.incrementBoost(id, amount);
 			amount = 0;
-			setIsInactive(true);
-			rechargeCoroutine = null;
+
+			setColor();
+
+			// Start recharge
+			rechargeCoroutine = StartCoroutine(recharge());
 		}
 
+		private void incrementAmount(float amt) {
+			amount += amt;
+
+			if (amount > MAX_AMT) {
+				amount = MAX_AMT;
+			}
+
+			Debug.Log(amount);
+
+			setColor();
+		}
+
+		// A player picks up a boost pad
+		// 2 second delay while the boostpad is completely inactive
+		// Then over the course of RECHARGE_TIME seconds, the boost regenerates to a maximum of 75% of MAX_AMT
+		// After RECHARGE_TIME seconds, the boost pad resets to 100%
 		private IEnumerator recharge() {
-			Debug.Log("Recharging");
-			yield return new WaitForSeconds(2);
+			yield return new WaitForSeconds(2); // Inactive for 2 seconds after pickup
 
-			int timer = RECHARGE_TIME;
+			float timer = RECHARGE_TIME;
 
-			timer--;
-
-			while (timer > 0) {
+			while (timer > 0.0f) {
 				// Leave the pickup off if the game has ended
 				if (!GameManager.getHasGameEnded()) {
 					// Leave the pickup off while time is paused
 					while (TimeManager.getIsPaused()) {
-						// NOTE: This is an imperfect solution. A player could
-						// 1) pick up a boost pad at time T and immediately pause the game,
-						// 2) wait until time T+d where d is the delay in time while a 
-						// boost pad replenishes and 3) unpause the game... in this case,
-						// the boost will _immediately_ replenish in the eyes 
-						// of the car
-						// 10/29 NOTE THIS MIGHT NOT BE TRUE ANYMORE, with the new recharge system
 						yield return null; 
 					}
 
-					amount += ((float)1/RECHARGE_TIME) * MAX_AMT;
+					incrementAmount((((float)1/RECHARGE_TIME) * (MAX_AMT * 0.75f)) / 10);
+					yield return new WaitForSeconds(0.1f);
 
-					setIsInactive(false);
-
-					Debug.Log(amount);
-
-					if (amount > MAX_AMT) {
-						amount = MAX_AMT;
-					}
-
-					yield return new WaitForSeconds(1);
-
-					timer--;
-				} else {
-					// Game has ended
-					StopCoroutine(rechargeCoroutine);
+					timer -= 0.1f;
+				} else { // Game has ended
+					stopRechargeCoroutine();
 				}
 			}
 
